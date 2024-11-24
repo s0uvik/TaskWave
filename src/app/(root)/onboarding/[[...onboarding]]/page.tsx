a02d6dfe96effe71275bf7fbbc1e7fc4d74a31e7
Souvik <souviksarkar260123@gmail.com>
2024-11-24 16:30:00 +0530

    add auto redirect to org

--- src/app/(root)/onboarding/[[...onboarding]]/page.tsx
+++ src/app/(root)/onboarding/[[...onboarding]]/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
-import React from "react";
-import { OrganizationList } from "@clerk/nextjs";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { OrganizationList, useOrganization } from "@clerk/nextjs";
 
 const Onboarding = () => {
+  const { organization } = useOrganization();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (organization) {
+      router.push(`/organization/${organization.slug}`);
+    }
+  }, [organization, router]);
+
   return (
     <div className=" flex justify-center items-center pt-12">
       <OrganizationList
